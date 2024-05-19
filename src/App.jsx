c894a38d9ee23079e9f2c1d2c85caed025465bd9
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-05-19 23:45:52 +0700

    Add dynamic player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import Player from './components/Player.jsx';
+
 function App() {
-	
-	
 	return (
-		<h1>React Tic-Tac-Toe</h1>
+		<main>
+			<div id="game-container">
+				<ol id="players">
+					<Player initialName="Player 1" symbol="X"></Player>
+					<Player initialName="Player 2" symbol="O"></Player>
+				</ol>
+			</div>
+		</main>
 	);
 }
 
